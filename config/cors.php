a0be89f1c920b3db3104e54103458510b21a8f4c
Mao sokchea <mao_sokchea@pp.bbu.edu.kh>
2026-08-08 13:37:11 +0700

    fix: clean up duplicate cors allowed_origins

--- config/cors.php
+++ config/cors.php
@@ -3,11 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [
-        'https://my-profolio-mao-sokchea.online',
-        'https://www.my-profolio-mao-sokchea.online',
-        'http://localhost:5173'
-    ],
+    'allowed_origins' => ['*'], // អនុញ្ញាតគ្រប់ Domain ទាំងអស់តែម្តង
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
