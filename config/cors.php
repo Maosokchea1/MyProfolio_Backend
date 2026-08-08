7f49037ef67abf601c65800759282ad8783607e3
Mao sokchea <mao_sokchea@pp.bbu.edu.kh>
2026-08-08 09:43:45 +0700

    Fix config files array return format

--- config/cors.php
+++ config/cors.php
@@ -1,16 +1,11 @@
 <?php
 
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Cross-Origin Resource Sharing (CORS) Configuration
     |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or CORS. This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
+    | ...
     */
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
@@ -28,5 +23,4 @@ return [
     'max_age' => 0,
 
     'supports_credentials' => false,
-
 ];
