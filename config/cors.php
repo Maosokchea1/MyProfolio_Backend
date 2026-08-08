1dfd53732ed88670e3c3ae845d1cee335be5d7e6
Mao sokchea <mao_sokchea@pp.bbu.edu.kh>
2026-08-08 10:34:37 +0700

    Update allowed origins for CORS

--- config/cors.php
+++ config/cors.php
@@ -1,18 +1,22 @@
 <?php
 
 return [
+
     /*
     |--------------------------------------------------------------------------
     | Cross-Origin Resource Sharing (CORS) Configuration
     |--------------------------------------------------------------------------
-    | ...
     */
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    // ជំនួសសញ្ញាផ្កាយ '*' ដោយដាក់ Domain របស់ Frontend របស់អ្នកនៅលើ Vercel
+    'allowed_origins' => [
+    'https://my-profolio-mao-sokchea.online',
+    'https://my-profolio-mao-sokchea.vercel.app'
+],
 
     'allowed_origins_patterns' => [],
 
@@ -22,5 +26,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    // ប្តូរពី false មកជា true ដើម្បីឱ្យវាទទួលយក Cookie ឬ Authentication tokens
+    'supports_credentials' => true,
 ];
